b2ac01b2ef5827a9f44865f55cca25ee14d59a4f
avallac <mail@avallac.ru>
2015-01-05 00:18:05 +0600

    Тараканы

--- Mobile/www/js/my-app.js
+++ Mobile/www/js/my-app.js
@@ -2,6 +2,7 @@
 var myApp = new Framework7();
 var hi, watchID;
 var ants = new Array();
+var dead,gone;
 
 // Export selectors engine
 var $$ = Dom7;
@@ -23,23 +24,32 @@ myApp.onPageInit('ant', function(){
     for(i=0; i<10; i++) {
         ants[i] =new Ant(i);
     }
-    setTimeout(moveAnt, 500);
+    gone = 0;
+    dead = 0;
+    setTimeout(moveAnt, 250);
 });
-function moveAnt () {
+
+function moveAnt ()
+{
     for(i=0; i<10; i++) {
         ants[i].move();
     }
-    setTimeout(moveAnt, 500);
+    $('#scoreInfo').html("<center><b>Раздавлено:</b>" + dead + "  <b>Сбежало:</b>" + gone+"</center>");
+    if((gone + dead) < 10) {
+        setTimeout(moveAnt, 250);
+    }
 }
 
 
-function onHeadingSuccess(heading) {
+function onHeadingSuccess(heading)
+{
     var hv = Math.round(heading.magneticHeading);
     hi.innerHTML = "<b>Направление:</b>" + hv + " градусов";
     $("#compass").rotate(-hv);
 }
 
-function onHeadingError(compassError) {
+function onHeadingError(compassError)
+{
     navigator.compass.clearWatchFilter(watchID);
     if(compassError.code == CompassError.COMPASS_NOT_SUPPORTED) {
         alert("Compass not supported.");
@@ -50,7 +60,8 @@ function onHeadingError(compassError) {
     }
 }
 
-function Ant(i) {
+function Ant(i)
+{
     this.id = i;
     this.dead = 0;
     this.distance = 0;
@@ -64,6 +75,11 @@ function Ant(i) {
             var newX = this.startX+Math.cos(this.orientation)*this.distance;
             var newY = this.startY+Math.sin(this.orientation)*this.distance;
             this.moveTo(newX,newY);
+            if(newX > $(window).width()+64 || newY > $(window).height()+64 || newY < -64 || newX < -64) {
+                this.dead = 1;
+                gone++;
+            }
+
         }
     }
     this.img = $("<img/>", {
@@ -72,10 +88,13 @@ function Ant(i) {
         height: 64,
         id: "ant_"+i,
         click: function(){
-             $(this).attr('src', "img/ant2.png");
-             $(this).css('z-index', -50);
-             $(this).rotate(0);
-             ants[this.id.substr(4)].dead = 1;
+             if(!ants[this.id.substr(4)].dead) {
+                 $(this).attr('src', "img/ant2.png");
+                 $(this).css('z-index', -50);
+                 $(this).rotate(0);
+                 ants[this.id.substr(4)].dead = 1;
+                 dead++;
+             }
         }
     }).appendTo("#kitchen");
     this.startY = Math.floor(Math.random()*($(window).height()-200))+50;
