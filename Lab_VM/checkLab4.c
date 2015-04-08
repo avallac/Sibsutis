11b66e1a9b554008a9010c600e6e3dae0a311c39
Petr Petrenko <mail@avallac.ru>
2015-04-08 09:53:31 +0600

    Lab4

---
+++ Lab_VM/checkLab4.c
@@ -0,0 +1,9 @@
+#include "libLab4.h"
+
+int main() {
+    mt_clrscr();
+    bc_box(1, 1, 10, 10);
+    printf("%d\n", sizeof(int));
+    return 0;
+}
+
---
+++ Lab_VM/libLab4.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <ncurses.h>
+
+int bc_printA (char * str);
+int bc_box(int x1, int y1, int x2, int y2);
